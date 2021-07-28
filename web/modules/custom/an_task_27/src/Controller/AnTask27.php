5bdf8ef9b5f80d62bc748e09f9c4d0c9172099dd
Andrey Nadezhkin <Andreikoman@gmail.com>
2021-07-28 11:44:30 +0300

    fixed notes in pull request

--- web/modules/custom/an_task_27/src/Controller/AnTask27.php
+++ web/modules/custom/an_task_27/src/Controller/AnTask27.php
@@ -8,22 +8,12 @@ namespace Drupal\an_task_27\Controller;
 class AnTask27 {
 
   /**
-   * Displays nodes.
+   * Gets content from database adn displays nodes.
+   *
+   * @return array
+   *   array of node elements to render
    */
   public function anTask27Content() {
-    return [
-      '#theme' => 'an-task-27',
-      '#nodes' => $this->anTask27QueryResult(),
-    ];
-  }
-
-  /**
-   * Gets content from database and creates HTML structure.
-   *
-   * @return string
-   *   String with HTML structure
-   */
-  private function anTask27QueryResult() {
     $query_result = \Drupal::database()
       ->select('node_field_data', 'n')
       ->fields('n', ['title', 'created'])
@@ -33,10 +23,13 @@ class AnTask27 {
       ->fetchAll();
 
     foreach ($query_result as $item) {
-      $item->created = date('Y-m-d H:i:s', $item->created);
+      $item->created = \Drupal::service('date.formatter')->format($item->created, 'short');
     }
 
-    return $query_result;
+    return [
+      '#theme' => 'an-task-27',
+      '#nodes' => $query_result,
+    ];
   }
 
 }
