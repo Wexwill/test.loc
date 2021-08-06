81b889df65864d29bdfa8f3dbb34bb046d603b58
Andrey Nadezhkin <Andreikoman@gmail.com>
2021-08-06 09:58:15 +0300

    added ajax to the form

--- web/modules/custom/an_task_32/src/Form/AnTask32Form.php
+++ web/modules/custom/an_task_32/src/Form/AnTask32Form.php
@@ -22,6 +22,7 @@ class AnTask32Form extends FormBase {
     $query->condition('vid', $vocabulary);
     $tids = $query->execute();
     $terms = Term::loadMultiple($tids);
+    $values['empty'] = NULL;
     foreach ($terms as $term) {
       $values[] = $term->name->value;
     }
@@ -44,12 +45,21 @@ class AnTask32Form extends FormBase {
       '#type' => 'select',
       '#title' => $this->t('Countries'),
       '#options' => $this->getTerms('country'),
+      '#validated' => TRUE,
+      '#ajax' => [
+        'callback' => '::countryCallback',
+        'event' => 'change',
+        'wrapper' => 'edit-city',
+      ],
     ];
 
     $form['city'] = [
       '#type' => 'select',
       '#title' => $this->t('Cities'),
-      '#options' => $this->getTerms('city'),
+      '#options' => [NULL],
+      '#prefix' => '<div id="edit-city">',
+      '#suffix' => '</div>',
+      '#validated' => TRUE,
     ];
 
     $form['submit'] = [
@@ -61,10 +71,42 @@ class AnTask32Form extends FormBase {
   }
 
   /**
+   * Contains callback function for Country field.
+   */
+  public function countryCallback(array &$form, FormStateInterface $form_state) {
+    $country = $form['country']['#options'][$form_state->getValue('country')];
+    $cities = $this->citiesArray($country);
+    $form['city']['#options'] = $cities;
+
+    return $form['city'];
+  }
+
+  /**
+   * Gets terms and creates array with key: City and value: Country.
+   *
+   * @return array
+   *   array with cities depends on selected country.
+   */
+  public function citiesArray($country) {
+    $query = \Drupal::entityQuery('taxonomy_term');
+    $query->condition('vid', 'city');
+    $tids = $query->execute();
+    $terms = Term::loadMultiple($tids);
+    foreach ($terms as $term) {
+      $array[$term->name->value] = $term->get('field_country')->entity->name->value;
+    }
+    $cities = array_keys($array, $country);
+
+    return $cities;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $message = 'Country: ' . $form_state->getValue('country') . ' | city: ' . $form_state->getValue('city');
+    $country = $form['country']['#options'][$form_state->getValue('country')];
+    $city = $this->citiesArray($country)[$form_state->getValue('city')];
+    $message = 'Country: ' . $country . ' | City: ' . $city;
     \Drupal::logger('an_task_32')->notice($message);
   }
 
